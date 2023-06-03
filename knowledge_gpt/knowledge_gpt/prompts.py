60196bc141e564356ba9c8f94db7ce86e4d139f4
CodeAdvisorAustralia <135187092+CodeAdvisorAustralia@users.noreply.github.com>
2023-06-04 03:55:26 +0500

    Update prompts.py

--- knowledge_gpt/knowledge_gpt/prompts.py
+++ knowledge_gpt/knowledge_gpt/prompts.py
@@ -2,7 +2,6 @@ from langchain.prompts import PromptTemplate
 
 ## Use a shorter template to reduce the number of tokens in the prompt
 template = """ 
-Never mention pages, instead highlight the relevant sections such as B1P1, follow the requirements of the question closely
 
 Always answer truthfully and if you dont find the relevant answer then tell that it isn't present in the building code
 
